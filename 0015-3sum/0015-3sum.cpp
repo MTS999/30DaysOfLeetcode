f8d5036ef744812a02a2356bb2fb092393b94aa1
Taimoor Shahid <101470872+MTS999@users.noreply.github.com>
2024-03-17 06:54:14 +0500

    Time: 64 ms (68.49%), Space: 29 MB (50.56%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+          sort(nums.begin(), nums.end());
+    vector<vector<int>> final;
+
+    int size = nums.size();
+
+    for (int i = 0; i < size; i++)
+    {
+
+        int target = -nums[i];
+        int front = i + 1;
+        int back = size - 1;
+        while (front < back)
+        {
+            int sum = nums[front] + nums[back];
+
+            if (sum < target)
+            {
+                front++;
+            }
+            else if (sum > target)
+            {
+                back--;
+            }
+            else
+            {
+                vector<int> triplet = {nums[i], nums[front], nums[back]};
+                final.push_back(triplet);
+                while (front < back && nums[front] ==triplet[1] ){ front++;}
+                while (front < back && nums[back] == triplet[2])  { back--;}
+
+            }
+        }
+        while (i + 1 < nums.size() && nums[i + 1] == nums[i])  i++;
+    }
+    return final;  
+    }
+};
